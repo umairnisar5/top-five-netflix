c15113444e15c48bc57c67a2b584cacbecbe86a8
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-23 01:54:04 +0500

    top  five net flix series with react

---
+++ src/Sdata.jsx
@@ -0,0 +1,25 @@
+const Sdata = [
+    {
+        imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRwUlzizz3RjK6hTg9cfTWoMU87Zsu2VVLExw&usqp=CAU",
+ title:"A Netflix Orignal series", 
+   snam:"DARK",
+   link:"https://www.netflix.com/pk/title/80100172"
+    },
+    {
+        imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSy_ESRytQHam5yW_puE4oZ6VU6N9J8NrbH0Q&usqp=CAU",
+ title:"A Netflix Orignal series", 
+   snam:"ERTUGRAL GHAZI",
+   link:"https://www.netflix.com/pk/title/80127001",
+    },
+
+    {
+        imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRwUlzizz3RjK6hTg9cfTWoMU87Zsu2VVLExw&usqp=CAU",
+ title:"A Netflix Orignal series", 
+   snam:"OSMAN GHAZI",
+   link:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQbw6bNHtuzp180_ZKw52cybiXtkFLPNV4hTg&usqp=CAU", 
+    }
+
+
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Card from "./Cards"
+import Sdat from "./Sdata";
+
+ 
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+
+<h1 className="head_style">List Of top 5 Netflix series in 2020 </h1>
+
+ <Card
+  imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRwUlzizz3RjK6hTg9cfTWoMU87Zsu2VVLExw&usqp=CAU"
+ title="A Netflix Orignal series" 
+   snam="DARK"
+   link="https://www.netflix.com/pk/title/80100172"
+ />
+ <Card
+  imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSy_ESRytQHam5yW_puE4oZ6VU6N9J8NrbH0Q&usqp=CAU"
+ title="A Netflix Orignal series" 
+   snam="ERTUGRAL GHAZI"
+   link="https://www.netflix.com/pk/title/80127001"
+ />
+ <Card
+  imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRwUlzizz3RjK6hTg9cfTWoMU87Zsu2VVLExw&usqp=CAU"
+ title="A Netflix Orignal series" 
+   snam="OSMAN GHAZI"
+   link="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQbw6bNHtuzp180_ZKw52cybiXtkFLPNV4hTg&usqp=CAU"
+ />
+  </>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
